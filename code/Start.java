2f201bf83de3a076470c538cf8373623a904912d
Voragun Supawong <voragun2543@gmail.com>
2022-08-24 20:54:33 +0700

    compare

--- code/Start.java
+++ code/Start.java
@@ -5,11 +5,33 @@ import java.util.Date;
 class Start {
     public static void main(String[] data) {       
         
-        Date p = new Date("2022/05/16");
-        Date q = new Date("2022/06/01");
-        Date r = new Date("2022/01/01");
+        CPU p = new CPU(2.5);
+        CPU q = new CPU(3.1);
+        CPU r = new CPU(2.2);
+        System.out.println( p.compareTo(q) );   // negative
+        System.out.println( p.compareTo(r) );   // positive
         
-        int c = p.compareTo(r);
-        System.out.println(c);        // positive value
+        CPU[] all = { p, q, r };
+        Arrays.sort(all);
+        for (int i = 0; i < all.length; i++) {
+            System.out.println(all[i].speed);
+        }
+    }
+}
+
+class CPU implements Comparable {
+    
+    double speed;
+    
+    CPU(double mhz) {
+        speed = mhz;
+    }
+    
+    @Override
+    public int compareTo(Object o) {
+        CPU x = (CPU)o;
+        if(this.speed < x.speed) return -1;
+        if(this.speed > x.speed) return +1;
+        return 0;
     }
 }
