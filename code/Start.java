5a822b7c80a1bf02a321d72c436f2e0250779024
Voragun Supawong <voragun2543@gmail.com>
2022-08-24 20:40:42 +0700

    compare date #2

--- code/Start.java
+++ code/Start.java
@@ -5,17 +5,11 @@ import java.util.Date;
 class Start {
     public static void main(String[] data) {       
         
-        Date[] all = {
-                new Date("1992/03/15"),
-                new Date("1992/01/27"),
-                new Date("1993/01/18"),
-                new Date("1992/05/01")
-        };
+        Date p = new Date("2022/05/16");
+        Date q = new Date("2022/06/01");
+        Date r = new Date("2022/01/01");
         
-        Arrays.sort(all);
-        for (int i = 0; i < all.length; i++) {
-            System.out.println(all[i]);
-        }
+        int c = p.compareTo(r);
+        System.out.println(c);        // positive value
     }
 }
-
